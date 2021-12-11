823f5bba7acdb6fc8c51defbf10e71df16c43786
youliangdao <youliangdaojing@gmail.com>
2021-12-11 15:32:17 +0900

    関数と変数を統合

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include "chibicc.h"
 
-int main(int argc, char const *argv[])
-{
-    if (argc != 2)
-      error("%s: invalid number of arguments", argv[0]);
+int main(int argc, char **argv) {
+  if (argc != 2)
+    error("%s: invalid number of arguments", argv[0]);
 
-    Token *tok = tokenize(argv[1]);
-    Function *prog = parse(tok);
+  // Tokenize and parse.
+  Token *tok = tokenize(argv[1]);
+  Obj *prog = parse(tok);
 
-    // Traverse the AST to emit assembly.
-    codegen(prog);
-    
-    return 0;
+  // Traverse the AST to emit assembly.
+  codegen(prog);
+
+  return 0;
 }
